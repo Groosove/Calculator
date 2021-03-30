8729aac1e27a3446b77a55449a2863d42eaebc4a
Groosove <arturianec2013@gmail.com>
2021-03-30 09:42:51 +0300

    start write functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,37 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	var result: Int = 0
+	var result: 		Int = 0
+	var leftValue: 		Int = 0
+	var rightValue: 	Int = 0
+	var chooseValue: 	Bool = true
+	var index: 			Int = 0;
 	
 	@IBOutlet weak var resultLabel: UILabel!
 	@IBOutlet weak var clearOutlet: UIButton!
-	@IBAction func Buttons(_ sender: UIButton) {
 	
-		let index = sender.tag
+	func takeValue(_ index: Int,_ value: inout Int) {
+		if (index == 17 && resultLabel.text != "0") {
+			value = -value
+		}
+		if (index >= 0 && index <= 9 && resultLabel.text?.count != 12) {
+			value = value * 10 + index
+		}
+	}
+	
+	@IBAction func Buttons(_ sender: UIButton) {
+		index = sender.tag
+		if (index >= 12 && index <= 15 || index == 18) {
+			chooseValue = !chooseValue
+		}
+		if ((index >= 0 && index <= 9) || index == 17) {
+			(chooseValue) ? takeValue(index, &leftValue) : takeValue(index, &rightValue)
+		}
 		
-		if (index == 0 && resultLabel.text == "0" || resultLabel.text?.count == 12) {
-			return
-		}
-		if (result == 0 && index > 0 && index <= 9) {
-			resultLabel.text = ""
-		}
+		result = leftValue + rightValue
 		if (sender.tag == 16 && result != 0) {
 			clearOutlet.setTitle("AC", for: .normal)
 			result = 0
+			leftValue = 0
+			rightValue = 0
+			resultLabel.text? = "0"
 		}
-		if (index == 17 && resultLabel.text != "0") {
-			result = -result
-		}
-		if (index >= 0 && index <= 9) {
-			result = result * 10 + index
-		}
+		
+
 		
 		if (result != 0) {
 			clearOutlet.setTitle("C", for: .normal)
 		}
 		resultLabel.text = String(result)
-		print(result)
 	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
